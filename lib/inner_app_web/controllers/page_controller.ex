c6aac5df21a4e89338dc24a74d2c0664c861b4d2
James Kim <james@sidenotes.xyz>
2023-09-02 21:02:29 +0900

    add maybe_put_outer_view

--- lib/inner_app_web/controllers/page_controller.ex
+++ lib/inner_app_web/controllers/page_controller.ex
@@ -4,6 +4,18 @@ defmodule InnerAppWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+
+    IO.inspect(conn.assigns)
+
+    conn
+    |> maybe_put_outer_view()
+    |> render(:home, layout: false)
   end
+
+  defp maybe_put_outer_view(%{assigns: %{outer_view: test_view}} = conn) do
+    conn
+    |> put_view(test_view)
+  end
+
+  defp maybe_put_outer_view(conn), do: conn
 end
